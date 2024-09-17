9883e1a4f9f4a318e3a2c6aeee9eb21e0cd51167
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-17 08:30:54 -0500

    feat: Created endpoint to get all data in "AnimalTypes"

--- Controllers/v1/AnimalTypes/AnimalTypesController.cs
+++ Controllers/v1/AnimalTypes/AnimalTypesController.cs
@@ -1,5 +1,8 @@
 using API_Farm.Data;
+using API_Farm.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace API_Farm.Controllers.v1.AnimalTypes;
 
@@ -13,4 +16,24 @@ public class AnimalTypesController : ControllerBase
     {
         Context = context;
     }
+
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Retrieves all animal types",
+        Description = "Gets a list of all animal types in the database."
+    )]
+    [SwaggerResponse(200, "Returns a list of animal types.", typeof(IEnumerable<AnimalType>))]
+    [SwaggerResponse(204, "There are no registered animal types.")]
+    [SwaggerResponse(500, "An internal server error occurred.")]
+    public async Task<IActionResult> GetAll()
+    {
+        var animalTypes = await Context.AnimalTypes.ToListAsync();
+
+        if (animalTypes.Any() == false)
+        {
+            return NoContent();
+        }
+        return Ok(animalTypes);
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using API_Farm.Data;
 using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 Env.Load();
 
@@ -21,7 +22,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Farm", Version = "v1" });
+        // Customize Swagger UI settings here.
+        c.EnableAnnotations();
+    }
+);
 
 var app = builder.Build();
 
