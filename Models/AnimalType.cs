e52ce4e4945e763e2272698d1d623835f4187c48
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-17 07:01:29 -0500

    feat: The class "AnimalType" was created

---
+++ Models/AnimalType.cs
@@ -0,0 +1,8 @@
+namespace API_Farm.Models;
+
+public class AnimalType
+{
+    public int Id { get; set; }
+    public required string Name { get; set; } 
+    public string? Description { get; set; } 
+}
