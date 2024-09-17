5588bfbb4520d1753c1a60b9ea9b7a27da1d19a4
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-17 07:05:30 -0500

    feat: The "AnimalType" class was configured for later creation in the database

--- Models/AnimalType.cs
+++ Models/AnimalType.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API_Farm.Models;
 
+[Table("animal_types")]
 public class AnimalType
 {
+    [Column("id")]
     public int Id { get; set; }
+
+    [Column("name")]
     public required string Name { get; set; } 
+    
+    [Column("description")]
     public string? Description { get; set; } 
 }
